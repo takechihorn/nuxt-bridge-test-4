350b396ede3978e6189a6d11e0fae27880aa6c51
近藤岳瑠 <takechihorn@gmail.com>
2022-01-05 16:04:50 +0900

    success vuetify and nuxt bridge

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import { defineNuxtConfig } from '@nuxt/bridge'
 
 export default defineNuxtConfig({
   // Disable server-side rendering: https://go.nuxtjs.dev/ssr-mode
-  ssr: false,
+  ssr: true,
 
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
